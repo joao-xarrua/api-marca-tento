3e2fda2ba91382bd5b7a53b42c267d2d8f7c57bd
joao vitor xarrua dos santos <joaoxarrua137@gmail.com>
2024-09-03 15:27:22 -0300

    CRUD and CORS allowed

--- Program.cs
+++ Program.cs
@@ -15,9 +15,33 @@ builder
         x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles; // previne os ciclos de acontecer
         x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault; // não vai renderizar objetos nulos
     }); // altera serialização padrão do system.text.json
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "http://www.apirequest.io")
+            // essas duas linhas são para poder fazer metodos além do GET
+            .AllowAnyHeader() 
+            .AllowAnyMethod();
+    }
+));
+
+
 builder.Services.AddDbContext<MarcaTentoDataContext>(); // adiciona o contexto do banco de dados
 
 var app = builder.Build();
+
+// precisa adicionar esse bolão de coisa até o "UseAuthorization" para remover o CORS e permitir algumas fontes
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
 
 app.MapControllers();
 
